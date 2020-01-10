b12f9fb0ef20b838f92dc02bd0f5a735ca88eb32
prashanth2 <prashanth2@users.noreply.github.com>
2020-01-10 13:14:42 -0600

    Update installnginx.sh

--- installnginx.sh
+++ installnginx.sh
@@ -4,4 +4,4 @@ sudo yum -y install nginx
 
 sudo systemctl start nginx
 
-echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /usr/share/nginx/html/index.html
